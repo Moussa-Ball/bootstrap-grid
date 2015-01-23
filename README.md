843e4e41e4e0ecfe9778787ececfc329c65b285a
Mikael Korpela <mikael@ihminen.org>
2015-01-23 19:04:58 +0200

    Bootstrap 3.3.2

---
+++ README.md
@@ -0,0 +1,28 @@
+# Bootstrap Grid
+
+One nice day I just needed a [Bootstrap](http://getbootstrap.com/) grid as a CSS file and nothing more.
+
+This is it.
+
+[Bootstrap Grid Tutorial](http://getbootstrap.com/css/#grid)
+
+[mikaelkorpela.fi](http://www.mikaelkorpela.fi/)
+
+## Usage
+Just include `dist/bootstrap-grid.css` or `dist/bootstrap-grid.min.css` to your project and start hacking.
+
+## Generate new versions
+To generate new non-minified and minified versions, run:
+```shell
+npm install
+bower install
+grunt
+```
+
+Normally you don't need to do this, they're in the repo already.
+
+## I need LESS/SASS version!
+Well, use the official Bootstrap repo, Luke! See [LESS](http://getbootstrap.com/css/#less) & [SASS](http://getbootstrap.com/css/#sass).
+
+## License
+[MIT](https://github.com/twbs/bootstrap/blob/master/LICENSE)
---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      default: {
+        options: {
+          paths: ['build/bootstrap-grid.css'],
+          compress: false,
+          sourceMap: true,
+          sourceMapFilename: 'dist/bootstrap-grid.css.map'
+        },
+        files: {
+          'dist/bootstrap-grid.css': 'bootstrap-grid.less'
+        }
+      },
+      minified: {
+        options: {
+          paths: ['build/bootstrap-grid.css'],
+          compress: true,
+          sourceMap: true,
+          sourceMapFilename: 'dist/bootstrap-grid.min.css.map'
+        },
+        files: {
+          'dist/bootstrap-grid.min.css': 'bootstrap-grid.less'
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('build', [
+                       'less:default',
+                       'less:minified',
+                     ]);
+
+    // Default task
+    grunt.registerTask('default', 'build');
+
+};
